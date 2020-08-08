6174a3386c6c5e513ee37eb4088f6fd6aea8648b
Joseph <jtawil2015@gmail.com>
2020-08-08 12:10:57 -0800

    created form and added modifying functions

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,16 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
+
+  const [form, setForm] = useState( {email: "", password: ""});
+  let example = form;
+  console.log(example);
+  const modifyEmail = (e)=>{
+    setForm({...example, email: e.target.value});
+  }
+
+  
+  const modifyPassword = (e)=>{
+   setForm({...example, password: e.target.value});
+  }
+
+
+
     return (
+      <div>
+        <div className="container">
         <form>
   <div className="form-group">
     <label htmlFor="exampleInputEmail1">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <input 
+    onChange={modifyEmail} 
+    type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div className="form-group">
+    <label htmlFor="exampleInputPassword1">Password</label>
+    <input onChange={modifyPassword} name="password" className="form-control" id="exampleInputPassword1"/>
   </div>
   <button type="submit" className="btn btn-primary">Submit</button>
 </form>
-  );
-};
+
+        </div>
+      </div>
+      
+    )
+}
 
 export default Form
