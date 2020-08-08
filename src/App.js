3f87be7345c0efccebc520d42de539028a1ccd27
Joseph <jtawil2015@gmail.com>
2020-08-08 10:31:07 -0800

    working with route and switch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
@@ -9,6 +10,20 @@ function App() {
        <Link to="/">Home</Link>
        <Link to="/about">About</Link>
        <Link to="/contact">Contact</Link>
+
+       <Switch>
+         <Route exact path = "/">
+           <h1>Hello From Home</h1>
+         </Route>
+         <Route exact path = "/about">
+           <h1>Hello From About</h1>
+         </Route>
+         {/* <Route exact path = "/contact">
+           <Form/>
+         </Route> */}
+        {/*This is another way of writing it*/}
+         <Route exact path = "/contact" component={Form}/>
+       </Switch>
      </Router>
     </div>
   );
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div>
+            <h1>This is not a form</h1>
+        </div>
+    )
+}
+
+export default Form
