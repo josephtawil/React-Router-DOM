7dcf6c4fb3488d0b899d118cf20ea346ac4f723c
Joseph <jtawil2015@gmail.com>
2020-08-08 10:20:20 -0800

    using link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <Router>
+       <Link to="/">Home</Link>
+       <Link to="/about">About</Link>
+       <Link to="/contact">Contact</Link>
+     </Router>
     </div>
   );
 }
