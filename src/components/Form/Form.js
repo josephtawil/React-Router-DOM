92fed54f190b42d83ed82b148272945bc9f07fc1
Joseph <jtawil2015@gmail.com>
2020-08-08 12:54:08 -0800

    worked with useStates

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,19 +1,42 @@
 import React, { useState } from 'react'
 
 const Form = () => {
+//   const thing = {
+//     first: "first value",
+//   };
+// ​
+//   const placeholder = "second";
+// ​
+//   thing[placeholder] = "oh wow";
+// ​
+//   console.log(thing);
+//   console.log(thing.first);
+
 
   const [form, setForm] = useState( {email: "", password: ""});
   let example = form;
   console.log(example);
-  const modifyEmail = (e)=>{
-    setForm({...example, email: e.target.value});
+  const modifyForm = (e)=>{
+    setForm({...example, [e.target.name]: e.target.value});
   }
 
   
-  const modifyPassword = (e)=>{
-   setForm({...example, password: e.target.value});
-  }
+  // const modifyPassword = (e)=>{
+  //  setForm({...example, password: e.target.value});
+  // }
 
+  const emailStyles = () =>{
+    let styles = "form-control";
+
+    if(form.email.includes("@"))
+    {
+      styles += " border-success"
+    }
+    else{
+      styles += " border-danger"
+    }
+    return styles;
+  }
 
 
     return (
@@ -23,18 +46,19 @@ const Form = () => {
   <div className="form-group">
     <label htmlFor="exampleInputEmail1">Email address</label>
     <input 
-    onChange={modifyEmail} 
-    type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    onChange={modifyForm} 
+    name="email" className={emailStyles()} id="exampleInputEmail1" aria-describedby="emailHelp"/>
     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div className="form-group">
     <label htmlFor="exampleInputPassword1">Password</label>
-    <input onChange={modifyPassword} name="password" className="form-control" id="exampleInputPassword1"/>
+    <input onChange={modifyForm} name="password" className="form-control" id="exampleInputPassword1"/>
   </div>
   <button type="submit" className="btn btn-primary">Submit</button>
 </form>
-
         </div>
+        <h1>{form.email}</h1>
+        <h1>{form.password}</h1>
       </div>
       
     )
